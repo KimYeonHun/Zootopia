ff51614b45fbc57c73c756897c51e753eb8e440c
Been0807 <iyab93@gmail.com>
2020-08-28 16:00:52 +0900

    member base workspace

---
+++ zootopia/src/main/java/com/kh/zootopia/entity/MemberDto.java
@@ -0,0 +1,16 @@
+package com.kh.zootopia.entity;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data@NoArgsConstructor@AllArgsConstructor@Builder
+public class MemberDto {
+	private String member_id,member_pw,birthday,
+	phone,auth,member_name,post,baseaddr,exrtaaddr,email,
+	gender;
+	
+}
