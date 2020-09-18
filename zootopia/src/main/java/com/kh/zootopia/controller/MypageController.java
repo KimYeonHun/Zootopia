c050423599736dc1be652888d70efde427e9e955
kyu-chang <chaung10821@naver.com>
2020-09-18 22:27:13 +0900

    수정 완료

--- zootopia/src/main/java/com/kh/zootopia/controller/MypageController.java
+++ zootopia/src/main/java/com/kh/zootopia/controller/MypageController.java
@@ -10,16 +10,16 @@ import java.util.List;
 
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
+
 import org.springframework.web.bind.annotation.GetMapping;
 
 import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
+
 
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
 
 import com.kh.zootopia.entity.MemberDto;
 import com.kh.zootopia.repository.MemberDao;
